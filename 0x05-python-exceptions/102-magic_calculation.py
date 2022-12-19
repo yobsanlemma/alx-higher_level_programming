5f39aa46cac32988ca18232a1ea5fd6df90ffe30
yobsanlemma <yobsanlemma13@gmail.com>
2022-12-19 20:55:04 +0300

    commit

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -13,3 +12,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return (result)
+
